720d9687a0fc240dae38e97e89c859141884c45a
JarkkoLietolahti <jarkko.lietolahti@gmail.com>
2016-02-18 14:48:43 +0000

    F*ck

--- files/printPdf.js
+++ files/printPdf.js
@@ -1,47 +1,165 @@
+/**
+ * Translate to current locale 
+ */
+t = function(key) {
+	
+	var ret =  TAPi18n.__(key);
+//	console.log("key="+ret);
+	return ret;
+	
+}
+
+// sweet. see 
+formatEur = function(value, format) {
+	let ret = new Intl.NumberFormat("fi-FI", {
+		style: 'currency',
+		currency: 'EUR'
+	}).format(value);
+
+	return ret;
+}
+
+formatDate = function(date, format) {
+	if (!date)
+		return "";
+
+	let ret = new Intl.DateTimeFormat("fi-FI").format(date);
+
+	return ret;
+}
+
+calculateCheckSum = function(referenceWithouCheckSum) {
+	let checkDigit = -1;
+
+	let multipliers = [7, 3, 1];
+	let multiplierIndex = 0;
+	let sum = 0;
+	
+	referenceWithouCheckSum = ""+referenceWithouCheckSum;
+
+	for (let i = referenceWithouCheckSum.length - 1; i >= 0; i--) {
+		if (multiplierIndex == 3) {
+			multiplierIndex = 0;
+		}
+
+		sum += referenceWithouCheckSum[i] * multipliers[multiplierIndex];
+		multiplierIndex++;
+	}
+
+	checkDigit = 10 - sum % 10;
+
+	if (checkDigit == 10) {
+		checkDigit = 0;
+	}
+
+	return checkDigit;
+}
+
+/* virtual barcode
+ * 1 Barcode version, this is version 4 or 5, 4 local finnish, 5 RE international version
+ * 1 Currency (1=FIM, 2=EURO)
+ * 16 IBAN without leading country code
+ * 6 Euros
+ * 2 Cents
+ * 3 Spares, contain zeros
+ * 20 Reference Number
+ * 6 Due Date. Format is YYMMDD.
+ */
+function createVirtualBarcode(IBAN_16, euros_6, cents_2, referenceNumber_20, dueDate_6) {
+	let virtualBarcode = "4" + IBAN_16 + euros_6 + cents_2 + "000" + referenceNumber_20 + dueDate_6;
+	return virtualBarcode;
+}
+
+
 printPdf = function(data) {
 
-	console.log("printPdf...", data);
-	// Define the pdf-document 
-	let companyName = "Kiinteistö Oy Lietolahti";
-	let companyAddress1 = "C/O Markku Lietolahti";
-	let companyAddress2 = "Heikkiläntie 13";
-	let companyAddress3 = "01900 NURMIJÄRVI";
+	// see https://github.com/lindell/JsBarcode
 
+	// console.log("printPdf...", data);
+	// Define the pdf-document 
+
+
+	// console.log("virtualbarcode=", virtualbarcode);
+
+	let invoice_details = data.invoice_details;
+
+	let date = formatDate(invoice_details.date);
+	let dueDate = formatDate(invoice_details.dueDate);
+
+	let ourReference = invoice_details.ourReference || " ";
+	let yourReference = invoice_details.yourReference || " ";
+	let invoiceNumber = invoice_details.invoiceNumber || " ";
+	let deliveryRange = invoice_details.deliveryRange || " ";
+	let paymentTerm = invoice_details.paymentTerm || " ";
+
+	let comp = data.invoice_details.company;
+
+	let companyName = comp.name || " ";
+	let companyAddress1 = comp.streetAddress || " ";
+	let companyAddress2 = comp.streetAddress2 || " ";
+	let companyAddress3 = comp.zipCode || " ";
+	let companyAddress4 = comp.city || " ";
+	let companyPhone = comp.phoneNumber || " ";
+	let companyIBAN = comp.IBAN || " ";
+	let companySWIFT = comp.SWIFT || " ";
+	let companyVATCode = comp.vatCode || " ";
+	let companyHomeplace = comp.homeplace || " ";
+	let companyEmail = comp.email || " ";
 
 	let customer = data.invoice_details.customer;
-	let customerName = customer.name;
-	let customerDetailOne = "customerDetailOne";
-	let customerDetailTwo = "customerDetailTwo";
-	let customerAdress1 = "Ruokakesko Oy";
-	let customerAdress2 = "Vuokrahallinto";
-	let customerAdress3 = "Satamakatu 3";
-	let customerAdress4 = "00016 KESKO";
+	// customer = Customers.findOne( {_id: customer.id });
 
-	let note = "Osuutenne lämmityskuluista Ihantolantie 12, Nurmijärvi";
+	let customerNumber = customer.customerNumber || "";
+
+	let customerName = customer.name || "";
+	let customerAdress1 = customer.streetAddress || "";
+	let customerAdress2 = customer.name2 || "";
+	let customerAdress3 = customer.zipCode || "";
+	let customerAdress4 = customer.city || "";
+
+	let note = "Note" || "";
+	let paymentReferenceNumber = "1123123125";
+
+
+
+	let line = {
+		table: {
+			headerRows: 1,
+			widths: ['100%'],
+			body: [
+				[''],
+				['']
+			]
+		},
+		layout: 'headerLineOnly'
+	};
+
 	let invoice_items = [];
 	let header = [{
 		bold: true,
-		text: "Nimike"
+		text: t("Nimike")
 	}, {
 		bold: true,
-		text: "Määrä"
+		text: t("Määrä")
 	}, {
 		bold: true,
-		text: "à hinta"
+		text: t("à hinta")
 	}, {
 		bold: true,
-		text: "Alv %"
+		text: t("Alv %")
 	}, {
 		bold: true,
-		text: "Yhteensä"
+		text: t("Yhteensä")
 	}];
 
 
 
 	invoice_items.push(header);
 
-	// console.log( data.invoice_items);
-	let totalAmount = 0;
+	// calclate VAT summaries 
+	let totalAmount = 0.0;
+	let vatSummary = {};
+
 	data.invoice_items.forEach(function(obj) {
 		let item = obj;
 		let row = [];
@@ -50,103 +168,232 @@ printPdf = function(data) {
 			text: "" + item.description
 		});
 		row.push("" + item.quantity);
-		row.push("" + item.price);
+		row.push("" + formatEur(item.price));
 		row.push("" + item.vat);
 		let rowSum = (item.quantity * item.price);
+
+		vatSummary[item.vat] = vatSummary[item.vat] ? vatSummary[item.vat] + rowSum : rowSum;
+
 		totalAmount += rowSum;
-		row.push("" + rowSum);
+		row.push("" + formatEur(rowSum));
 		// console.log(row);
 		invoice_items.push(row);
 	});
 
-	let footer = ["", "", "", "", ""+totalAmount];
-	invoice_items.push(footer);
-	console.log(invoice_items.slice(0));
 
-	let currentUser = "Printed by User " + Meteor.user().toString();
+	let vatFooter = [
+		["Alv%", "Netto", "Vero", "Brutto"]
+	];
+
+	for (let vat in vatSummary) {
+
+		let brutto = vatSummary[vat];
+		let vatFactor = (parseFloat(vat) + 100) / 100;
+		let taxAmount = brutto - (brutto / vatFactor);
+		let netto = brutto - taxAmount;
+		let vatRow = [];
+		// console.log( "alv="+vat+" netto="+netto+" vero="+taxAmount+" brutto="+brutto);
+		vatRow.push("" + vat);
+		vatRow.push("" + formatEur(netto));
+		vatRow.push("" + formatEur(taxAmount));
+		vatRow.push("" + formatEur(brutto));
+
+		vatFooter.push(vatRow);
+	}
+
+	// invoice rows summary
+	let footer = ["", "", "", {
+		bold: true,
+		text: t("Yhteensä")
+	}, {
+		bold: true,
+		text: "" + formatEur(totalAmount)
+	}];
+
+	invoice_items.push(footer);
+
+
+	// console.log(invoice_items.slice(0));
+	// calculate reference numbers
+
+	if ($("#barcode").length == 0) {
+		$(".printPdf").after("<img id='barcode' ></img>");
+		$("#barcode").hide();
+	}
+
+	/* virtual barcode
+	 * 1 Barcode version, this is version 4 or 5, 4 local finnish, 5 RE international version
+	 * 1 Currency (1=FIM, 2=EURO)
+	 * 16 IBAN without leading country code
+	 * 6 Euros
+	 * 2 Cents
+	 * 3 Spares, contain zeros
+	 * 20 Reference Number
+	 * 6 Due Date. Format is YYMMDD.
+	 */
+	// console.log(totalAmount);
+	// remove first two characters and any spaces
+	let tempIBAN = companyIBAN.slice(2).replace(/ /g, "");
+	// totalAmount is float here
+	// 970.08
+	let tempSum = sprintf("%09.2f", totalAmount);
+	tempSum = tempSum.split(".");
+	let euros = tempSum[0];
+	let sents = tempSum[1];
+	// console.log("euros="+euros+" sents="+sents);
+	let tempRef = sprintf("%020d", paymentReferenceNumber);
+	let tempDueDate = moment(invoice_details.dueDate).format("YYMMDD");
+
+	let virtualBarcode = "4" + tempIBAN + euros + sents + "000" + tempRef + tempDueDate;
+
+	// var barcode = JsBarcode("#barcode","JsBarcode is easy!",{width:1,height:25});
+	let barcode = $("#barcode").JsBarcode(virtualBarcode, {
+		height: 30,
+		width: 1,
+		format: "CODE128C",
+		displayValue: true,
+		fontSize: 10
+	});
+
+	// console.log("barcode=", barcode);
+	let virtualbarcode = barcode[0].currentSrc;
+
+
 
 	var docDefinition = {
 		pageSize: 'A4',
-		pageMargins: [30, 25, 30, 25],
+		pageMargins: [50, 25, 50, 25],
 
 		// Content with styles 
 		content: [{
-			text: companyName,
-			style: 'headline'
-		}, {
-			columns: [{
-				width: '35%',
-				style: ['listItem'],
-				stack: [
-					companyAddress1,
-					companyAddress2,
-					companyAddress3
-				]
+				text: companyName,
+				style: 'headline'
 			}, {
-				width: '20%',
-				text: [{
-					style: "listLabel",
-					text: "Lasku\n"
-				}, "Päiväys\n", "1.1.2016"],
-				style: ['listItem']
-			}, {
-				width: '*',
-				text: [{
-					style: "listLabel",
-					text: "Lasku nro\n"
-				}, "#1001"],
-				style: ['listItem']
-			}],
-			columnGap: 10
-		}, {
-			text: "\n\n"
-		}, {
-			columns: [{
-				width: '35%',
-				style: ['listItem'],
-				text: [{
+				columns: [{
+					width: '35%',
+					style: ['listItem'],
+					stack: [
+						companyAddress1,
+						companyAddress2,
+						companyAddress3 + " " + companyAddress4
+					]
+				}, {
+					width: '20%',
+					stack: [{
 						style: "listLabel",
-						text: "Laskutusasiakas"
-					},
-					"\n",
-					customerAdress1,
-					"\n",
-					customerAdress2,
-					"\n",
-					customerAdress3,
-					"\n",
-					customerAdress4
-				]
-			}, {
-				width: '20%',
-				text: [{
-					style: "listLabel",
-					text: "Laskutusasiakas\n"
-				}, "Maksuehto\n", "Eräpäivä\n", "\n\n", "Viitteemme\n", "Viitteenne\n", "\n", "Toimitusaika"],
-				style: ['listLabel']
-			}, {
-				width: '*',
-				text: ["24 pv netto\n", "22.05.2015\n", "\n\n", "Nurmijärven Sähkö Oy\n", "Vuokrasop. 12.4.2012\n", "\n", "1.1.-31.1.2013"],
-				style: ['listItem']
-			}],
-			columnGap: 10
-		}, {
-			text: "\n\n"
-		}, {
-			text: note,
-		}, {
-			text: "\n"
-		}, { // invoice_items rows
-			table: {
-				headerRows: 1,
-				widths: ['*', 'auto', 'auto', 'auto', 'auto'],
+						text: t("Lasku"),
+						fontSize: 16
+					}, t("Päiväys"), "" + date],
+					style: ['listItem']
 
-				body: invoice_items
-
-
+				}, {
+					width: '*',
+					text: [{
+						style: "listLabel",
+						text: t("Lasku nro")+"\n"
+					}, invoiceNumber],
+					style: ['listItem']
+				}],
+				columnGap: 10
 			},
-			layout: 'lightHorizontalLines'
-		}],
+			line, {
+				columns: [{
+					width: '35%',
+					style: ['listItem'],
+					stack: [{
+							style: "listLabel",
+							text: t("Laskutusasiakas")
+						},
+						customerName,
+						customerAdress1,
+						customerAdress2,
+						customerAdress3 + " " + customerAdress4
+					]
+				}, {
+					width: '20%',
+					stack: [t("Asiakasnumero"), t("Maksuehto"), "\n", t("Eräpäivä"), t("Viitenumero"), "\n", t("Viitteemme"), t("Viitteenne"), t("Toimitusaika")],
+					style: ['listItem']
+				}, {
+					width: '*',
+					stack: [customerNumber, paymentTerm, "\n", "" + dueDate, paymentReferenceNumber, "\n",
+						ourReference, yourReference, deliveryRange
+					],
+					style: ['listItem']
+				}],
+				columnGap: 10
+			}, {
+				text: "\n"
+			}, {
+				text: note,
+				style: ['listItem']
+			}, {
+				text: "\n"
+			}, { // invoice_items rows
+				table: {
+					headerRows: 1,
+					widths: ['*', 'auto', 'auto', 'auto', 'auto'],
+					style: ['listItem'],
+					body: invoice_items
+
+
+				},
+				layout: 'lightHorizontalLines',
+				style: ['listItem']
+			}, {
+				text: "\n\n"
+			}, {
+				style: ['listItem'],
+				table: {
+					headerRows: 1,
+					widths: ['auto', 'auto', 'auto', 'auto'],
+					alignment: 'right',
+					body: vatFooter
+				}
+			},
+
+			line, {
+				margin: [0, 0, 0, 0],
+				style: ['footer'],
+				columns: [{
+					width: "auto",
+					stack: [companyName, companyAddress1, companyAddress2, companyAddress3 + " " + companyAddress4]
+				}, {
+					width: "auto",
+					stack: [{
+						bold: true,
+						text: t("Puhelin")
+					}, companyPhone, {
+						bold: true,
+						text: t("Email")
+					}, companyEmail]
+				}, {
+					width: "auto",
+					stack: [{
+						bold: true,
+						text: t("Pankit")
+					}, companyIBAN, companySWIFT]
+				}, {
+					width: "auto",
+					stack: [{
+						columns: [{
+							margin: [0, 0, 0, 0],
+							bold: true,
+							stack: [t("Y-tunnus"), t("Kotipaikka")],
+							alignment: "right"
+						}, {
+							stack: [companyVATCode, companyHomeplace]
+						}]
+
+					}]
+				}],
+				columnGap: 10
+			},
+			line, {
+				image: virtualbarcode,
+				alignment: "center"
+			}
+
+		],
 
 		// Style dictionary 
 		styles: {
@@ -166,6 +413,9 @@ printPdf = function(data) {
 			listText: {
 				fontSize: 10,
 				italic: true
+			},
+			footer: {
+				fontSize: 8
 			}
 		}
 	};
