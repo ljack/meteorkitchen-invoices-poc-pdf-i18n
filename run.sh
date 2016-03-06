14423a382f9c8bcfe46b45671cd23f81332a3675
JarkkoLietolahti <jarkko.lietolahti@gmail.com>
2016-03-06 09:40:58 +0000

    Autocommit

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ export METEOR_WATCH_FORCE_POLLING=1
 export METEOR_WATCH_POLLING_INTERVAL_MS=1000
 
 cd laskuni
-meteor -p $IP:$PORT --settings files/settings.json
+meteor -p $IP:$PORT --settings settings.json
 cd ..
